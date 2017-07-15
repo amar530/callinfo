c6e981b1381f9a2d00d41ab571430ed4ef776fa7
amar530 <er.amar1709@gmail.com>
2017-07-15 21:55:08 +0530

    updated the project

--- js/angular.js
+++ js/angular.js
@@ -1,11 +1,19 @@
-// var cors=new enableCorsAttribute("*","*","*");
-// config.EnableCors(cors);
 
-var app = angular.module('myApp', []);
-app.controller('cntr', function($scope, $http) {
+app = angular.module('myApp', []);
+ app.controller('cntr', function($scope, $http) {
+     app.config(function($httpProvider) {
+      //Enable cross domain calls
+      $httpProvider.defaults.useXDomain = true;
+
+      //Remove the header used to identify ajax call  that would prevent CORS from working
+//      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  });
     $http({
         method : "GET",
-        url : "http://ec2-13-126-231-175.ap-south-1.compute.amazonaws.com:8080/pc/dummyList"
+        url : "http://ec2-13-126-231-175.ap-south-1.compute.amazonaws.com:8080/pc/dummyList",
+         headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+        }
     }).then(function mySuccess(response) {
         $scope.datas = response.data;
         // var datass= $scope.datas;
@@ -13,11 +21,8 @@ app.controller('cntr', function($scope, $http) {
         {	
         		$scope.txt=amar;	
         		// console.log(amar);
-        	
         }
     }, function myError(response) {
         $scope.myWelcome = response.statusText;
     });
 });
-var cors=new enableCorsAttribute("*","*","*");
-config.EnableCors(cors);
